0cddcf5f6467455c64cd7bbfcd7ce7d7fdbf135f
ARUN KUMAR <aim2achieve002@gmail.com>
2015-03-22 15:11:09 +0530

    Update hackuser.js
    This is my solution for the problem.

--- src/hackuser.js
+++ src/hackuser.js
@@ -14,6 +14,29 @@ if (result['status']) {
 hackAlgorithmToFindUsername(result['encrypted']);
 
 function hackAlgorithmToFindUsername(encrypterPass) {
-  // complete the hack to find the username for any encrypted password from authorizer.js.
-  throw new Error("Missing hack to find username");
+array_elements=(encrypterPass.split("").sort());
+    var current = null;
+    var cnt = 0;
+    var finalResult = new Array(array_elements.length);
+    for (var i = 0; i < array_elements.length; i++) {
+        if (array_elements[i] != current) {
+            if (cnt > 0) {
+                //console.log(current,cnt);
+var div1=cnt/2;
+for(j=0;j<div1;j++){finalResult.push(current);}
+            }
+            current = array_elements[i];
+            cnt = 1;
+        } else {
+            cnt++;
+        }
+    }
+    if (cnt > 0) {
+        //console.log(current,cnt);
+var div2=cnt/2;
+for(k=0;k<div2;k++){finalResult.push(current);}
+    }
+console.log('ENCRYPTED Password :',encrypterPass );
+console.log('******PASSWORD DECRYPTED******');
+console.log('User Name :',finalResult.join(''));
 }
