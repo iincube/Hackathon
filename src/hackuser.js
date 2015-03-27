b551c5b71a75f9124928dd04c809a317ae4e6146
sridhar <sridhar@pixalate.com>
2015-03-27 18:54:58 +0530

    Optimized the time constraint

--- src/hackuser.js
+++ src/hackuser.js
@@ -4,6 +4,7 @@ var auth = new UserAuth({
 	'username': 'aaaadhiimnrrrssv',
 	'password': 'sridharsivaraman'
 });
+
 var result = auth.authenticate();
 
 if (result['status']) {
@@ -15,6 +16,29 @@ if (result['status']) {
 
 hackAlgorithmToFindUsername(result['encrypted']);
 
-function hackAlgorithmToFindUsername(encrypterPass) {
-    console.log((encrypterPass.split('').sort().join('')).replace(/(.){2}/g, '$1'));
+function hackAlgorithmToFindUsername(encrypted_password) {
+
+    console.log("ENCRYPTED PASSWORD: " + encrypted_password);
+
+    var encrypted_array = encrypted_password.split('');
+
+    var max_bits = 8;
+
+    for (var d = 0; d < max_bits; ++d) {
+        for (var i = 0, p = 0, b = 1 << d, n = encrypted_array.length; i < n; ++i) {
+            var o = encrypted_array[i].charCodeAt(0); // GET ASCII for sorting
+            if ((o & b) == 0) {
+                encrypted_array.splice(p++, 0, encrypted_array.splice(i, 1)[0]);
+            }
+        }
+    }
+
+    var result_array = [];
+    for(var i = 0; i < encrypted_array.length;i = i + 2) {
+       result_array.push(encrypted_array[i]);
+    }
+
+    console.log("HACKED USERNAME: " + result_array.join(''));
+    
 }
+
